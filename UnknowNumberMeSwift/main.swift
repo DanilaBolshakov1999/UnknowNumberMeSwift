1717519eedd3065c8593b080e5b4f1c96b6d10c2
Danila Bolshakov <danila1999777@gmail.com>
2023-05-06 02:07:22 +0300

    release v2

--- UnknowNumberMeSwift/main.swift
+++ UnknowNumberMeSwift/main.swift
@@ -10,5 +10,17 @@ import Foundation
 // MARK: - Constants
 
 let randomNumber = Int.random(in: 1...250) //задаем рандомный диапазон значений
+print("Введите число от 1 до 250!") //print for console
+var number: String?
 
-
+repeat {
+    print("Введите число и нажмите Return")
+    number = readLine()
+    if randomNumber == Int(number!) {
+        print("Вы ввели верное число! Поздравляю Вас!")
+    } else if randomNumber > Int(number!)! {
+        print("Вы ввели число больше! Попробуйте еще раз!")
+    } else if randomNumber < Int(number!)! {
+        print("Вы ввели число меньше! Попробуйте еще раз!")
+    }
+} while randomNumber != Int(number!)
