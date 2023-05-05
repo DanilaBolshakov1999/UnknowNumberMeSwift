6f5058b55a88e3d3824b0621e09486c9b7a588ab
Danila Bolshakov <danila1999777@gmail.com>
2023-05-06 01:42:23 +0300

    release v1

---
+++ UnknowNumberMeSwift/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  UnknowNumberMeSwift
+//
+//  Created by IOS - Developer  on 06.05.2023.
+//
+
+import Foundation
+
+// MARK: - Constants
+
+let randomNumber = Int.random(in: 1...250) //задаем рандомный диапазон значений
+
+
