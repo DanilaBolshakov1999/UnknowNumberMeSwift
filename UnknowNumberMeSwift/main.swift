1e504a0eb615a68c8ed0f8fd111df73b914e0283
Danila Bolshakov <danila1999777@gmail.com>
2023-05-06 15:28:19 +0300

    release v5

--- UnknowNumberMeSwift/main.swift
+++ UnknowNumberMeSwift/main.swift
@@ -14,11 +14,12 @@ print("Enter a number option from 1 to 250 and press return") //print in console
 var isWin = false //флаг указатель на результат
 
 repeat { //условия с false использованием
-    guard let userNumber = UInt8(readLine() ?? "") else {
+    guard let userNumber = UInt8(readLine() ?? "") else { //если условие false то используем тело условия
         print("You have entered an invalid value! Try again!")
-        continue
+        continue //переходим к новой итерации
     }
     
+    //Проверка условия if else {}
     if userNumber < randomNumber {
         print("You have entered a value less than! Try again!")
     } else if userNumber > randomNumber {
@@ -27,6 +28,6 @@ repeat { //условия с false использованием
         print("You win!")
         isWin = true
     }
-} while !isWin
+} while !isWin //пока, while isWin = true
 
 
